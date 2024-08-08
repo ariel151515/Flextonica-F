9fe560697eff30aba9804c15f361b99437963a34
ariel151515 <ndcdigital2020@gmail.com>
2024-08-08 09:20:42 -0300

    Box Diario mejorado con input

--- Flextonica/src/components/BoxDiario.tsx
+++ Flextonica/src/components/BoxDiario.tsx
@@ -8,76 +8,116 @@ export const BoxDiario: React.FC<BoxDiarioProps> = () => {
     <View className='shadow bg-white' style={styles.container}>
 
       <View className='flex flex-row justify-between bg-black'>
-        <View className='bg-amarillo pt-2 ' style={{width:'24%'}}>
-          <Text className='text-blak font-semibold text-base pl-5'>Diario</Text>
+        <View className='bg-amarillo pt-2 ' style={{width:'24%', paddingLeft:20}}>
+          <TextInput>
+             <Text className='text-blak font-semibold text-base pl-5'>Diario</Text>
+          </TextInput>
         </View>
         <View className='bg-black px-4 h-10 justify-center flex-1'>
-          <Text className='text-white text-center font-sans text-base'>KCAL</Text>
+          <TextInput style={{marginLeft:10}}>
+             <Text className='text-white text-center font-sans text-base'>KCAL</Text>
+          </TextInput>
         </View>
         <View className='bg-black px-3 h-10 justify-center flex-1'>
-          <Text className='text-white text-center font-sans text-base'>PR</Text>
+          <TextInput style={{marginLeft:10}}>
+              <Text className='text-white text-center font-sans text-base'>PR</Text>
+          </TextInput>
         </View>
         <View className='bg-black px-3 h-10 justify-center flex-1'>
-          <Text className='text-white text-center font-sans text-base'>CH</Text>
+          <TextInput style={{marginLeft:10}}>
+             <Text className='text-white text-center font-sans text-base'>CH</Text>
+          </TextInput>
         </View>
         <View className='bg-black px-3 h-10 justify-center flex-1'>
-          <Text className='text-white text-center font-sans text-base'>GR</Text>
+          <TextInput style={{marginLeft:10}}>
+              <Text className='text-white text-center font-sans text-base'>GR</Text>
+          </TextInput>
         </View>
       </View>
 
-      <View className='flex flex-row justify-betweene'>
-        <View className='px-5 h-10 justify-center w-31'>
-          <Text className='text-black text-center text-base font-semibold'>Objetivo</Text>
+      <View className='flex flex-row justify-between bg-black'>
+        <View className='bg-white pt-2 ' style={{width:'24%', paddingLeft:20}}>
+          <TextInput>
+             <Text className='text-blak font-semibold text-base pl-5'>Objetivo</Text>
+          </TextInput>
         </View>
-        <View className='justify-center' style={{width:'20%'}}>
-        <Text className='text-black text-center font-semibold text-base'>1234</Text>
+        <View className='bg-white px-4 h-10 justify-center flex-1'>
+          <TextInput style={{marginLeft:10}}>
+             <Text className='text-blak text-center font-semibold text-base'>1340</Text>
+          </TextInput>
         </View>
-        <View className='px-5 h-10 justify-center flex-1'>
-        <Text className='text-black text-center font-semibold text-base'>156</Text>
+        <View className='bg-white px-3 h-10 justify-center flex-1'>
+          <TextInput style={{marginLeft:10}}>
+              <Text className='text-blak text-center font-semibold text-base'>120</Text>
+          </TextInput>
         </View>
-        <View className='px-5 h-10 justify-center flex-1'>
-        <Text className='text-black text-center font-semibold text-base'>345</Text>
+        <View className='bg-white px-3 h-10 justify-center flex-1'>
+          <TextInput style={{marginLeft:10}}>
+             <Text className='text-blak text-center font-semibold text-base'>345</Text>
+          </TextInput>
         </View>
-        <View className=' px-5 h-10 justify-center flex-1'>
-        <Text className='text-black text-center font-semibold text-base'>56</Text>
+        <View className='bg-white px-3 h-10 justify-center flex-1'>
+          <TextInput style={{marginLeft:10}}>
+              <Text className='text-blak text-center font-semibold text-base'>58</Text>
+          </TextInput>
+        </View>
+      </View>
+    
+      <View className='flex flex-row justify-between bg-black'>
+        <View className='bg-white pt-2 ' style={{width:'24%', paddingLeft:20}}>
+          <TextInput>
+             <Text className='text-blak font-semibold text-base pl-5'>Totales</Text>
+          </TextInput>
+        </View>
+        <View className='bg-white px-4 h-10 justify-center flex-1'>
+          <TextInput style={{marginLeft:10}}>
+             <Text className='text-blak text-center font-sans text-base'>1340</Text>
+          </TextInput>
+        </View>
+        <View className='bg-white px-3 h-10 justify-center flex-1'>
+          <TextInput style={{marginLeft:10}}>
+              <Text className='text-blak text-center font-sans text-base'>120</Text>
+          </TextInput>
+        </View>
+        <View className='bg-white px-3 h-10 justify-center flex-1'>
+          <TextInput style={{marginLeft:10}}>
+             <Text className='text-blak text-center font-sans text-base'>345</Text>
+          </TextInput>
+        </View>
+        <View className='bg-white px-3 h-10 justify-center flex-1'>
+          <TextInput style={{marginLeft:10}}>
+              <Text className='text-blak text-center font-sans text-base'>58</Text>
+          </TextInput>
         </View>
       </View>
 
-      <View className='flex flex-row justify-betweene'>
-        <View className='px-5 h-10 justify-center w-31'>
-          <Text className='text-black text-center text-base font-semibold'>Totales<Text className='text-white'>_</Text></Text>
+       <View className='flex flex-row justify-between bg-black'>
+        <View className='bg-white pt-2 ' style={{width:'24%', paddingLeft:20}}>
+          <TextInput>
+             <Text className='text-blak font-semibold text-base pl-5'>Restantes</Text>
+          </TextInput>
         </View>
-        <View className='justify-center' style={{width:'20%'}}>
-        <Text className='text-black text-center font-sans text-base'>1234</Text>
+        <View className='bg-white px-4 h-10 justify-center flex-1'>
+          <TextInput style={{marginLeft:10}}>
+             <Text className='text-blak text-center font-sans text-base'>1340</Text>
+          </TextInput>
         </View>
-        <View className='px-5 h-10 justify-center flex-1'>
-        <Text className='text-black text-center font-sans text-base'>156</Text>
+        <View className='bg-white px-3 h-10 justify-center flex-1'>
+          <TextInput style={{marginLeft:10}}>
+              <Text className='text-blak text-center font-sans text-base'>120</Text>
+          </TextInput>
         </View>
-        <View className='px-5 h-10 justify-center flex-1'>
-        <Text className='text-black text-center font-sans text-base'>345</Text>
+        <View className='bg-white px-3 h-10 justify-center flex-1'>
+          <TextInput style={{marginLeft:10}}>
+             <Text className='text-blak text-center font-sans text-base'>345</Text>
+          </TextInput>
         </View>
-        <View className=' px-5 h-10 justify-center flex-1'>
-        <Text className='text-black text-center font-sans text-base'>56</Text>
+        <View className='bg-white px-3 h-10 justify-center flex-1'>
+          <TextInput style={{marginLeft:10}}>
+              <Text className='text-blak text-center font-sans text-base'>58</Text>
+          </TextInput>
         </View>
-      </View>
-
-      <View className='flex flex-row justify-betweene'>
-        <View className='px-5 h-10 justify-center w-31'>
-          <Text className='text-black text-center text-base font-semibold'>Restan<Text className='text-white'>..</Text></Text>
-        </View>
-        <View className='justify-center' style={{width:'20%'}}>
-        <Text className='text-black text-center font-sans text-base'>1234</Text>
-        </View>
-        <View className='px-5 h-10 justify-center flex-1'>
-        <Text className='text-black text-center font-sans text-base'>156</Text>
-        </View>
-        <View className='px-5 h-10 justify-center flex-1'>
-        <Text className='text-black text-center font-sans text-base'>345</Text>
-        </View>
-        <View className=' px-5 h-10 justify-center flex-1'>
-        <Text className='text-black text-center font-sans text-base'>56</Text>
-        </View>
-      </View>
+      </View> 
 
     </View>
   );
